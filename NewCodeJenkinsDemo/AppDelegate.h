e02b34a918af56199e1b17f245c20a446b7d97fb
Bharat <bharatshankar5@gmail.com>
2019-09-11 12:01:52 +0530

    Initial Commit

---
+++ NewCodeJenkinsDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewCodeJenkinsDemo
+//
+//  Created by Sunil.Godhumala on 11/09/19.
+//  Copyright © 2019 Sunil.Godhumala. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NewCodeJenkinsDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewCodeJenkinsDemo
+//
+//  Created by Sunil.Godhumala on 11/09/19.
+//  Copyright © 2019 Sunil.Godhumala. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
